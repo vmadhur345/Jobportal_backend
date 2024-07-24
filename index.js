dbc759fcdf9fed7f2f4cf28084e048c12480a142
Madhur Verma <vmadhur345@gmail.com>
2024-07-24 16:03:36 +0530

    changes in CORS origin

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 const corsOptions = {
-    origin: ['https://job-find-post.netlify.app', 'http://localhost:5173'],
+    origin: ['https://guileless-cucurucho-62b112.netlify.app', 'http://localhost:5173'],
     credentials:true
 }
 app.use(cors(corsOptions))

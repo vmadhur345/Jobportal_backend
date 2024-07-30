ee49b08ded9d0d26a80f53f41364ad98ae0baf49
Madhur Verma <vmadhur345@gmail.com>
2024-07-30 16:52:23 +0530

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://effervescent-taiyaki-eab2e6.netlify.app/'],
+    origin: ['http://localhost:5173', 'https://effervescent-taiyaki-eab2e6.netlify.app'],
     credentials: true,
 };
 

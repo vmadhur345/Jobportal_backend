4a59a9b599a24c205bb02a2e517d697ae5c0ba9a
Madhur Verma <vmadhur345@gmail.com>
2024-07-30 16:40:04 +0530

    first commit

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: ['http://localhost:5173', 'https://jobportal-backend-m6fv.onrender.com'],
+    credentials: true,
+};
+
 
 app.use(cors(corsOptions));
 

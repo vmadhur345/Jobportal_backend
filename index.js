30cd543e3f16f9bab6b6bff8cf4a65622f185f02
Madhur Verma <vmadhur345@gmail.com>
2024-08-18 13:23:57 +0530

    changes in cors

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://lucky-cocada-edc01a.netlify.app'],
     credentials: true,
 };
+
 
 
 app.use(cors(corsOptions));
